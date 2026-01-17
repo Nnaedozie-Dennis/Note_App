04dde0373c9374fa3f4c91a63d9e7e6d4d2c6ade
Nnaedozie Dennis <dennisnnaedozie@gmail.com>
2026-01-17 07:01:54 +0100

    Update HomeButtons

--- frontend/src/app/components/HomeButtons.js
+++ frontend/src/app/components/HomeButtons.js
@@ -1,11 +1,14 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from 'react';
 
 export default function HomeButtons() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const handleDemoLogin = async () => {
+    setLoading(true);
     try {
       const res = await fetch(
         "https://note-app-k88k.onrender.com/api/auth/demo",
@@ -25,6 +28,8 @@ export default function HomeButtons() {
     } catch (err) {
       alert("Network error. Check if backend is running on port 5000.");
       console.error(err);
+    } finally {
+      setLoading(false); 
     }
   };
 
@@ -32,9 +37,19 @@ export default function HomeButtons() {
     <div className="flex flex-col sm:flex-row gap-4 sm:gap-8 justify-center items-center w-full">
       <button
         onClick={handleDemoLogin}
+        disabled={loading}
         className="w-full sm:w-auto px-6 sm:px-10 py-3 bg-linear-to-r from-indigo-600 to-indigo-800 text-white sm:py-5 text-lg sm:text-xl font-bold rounded-xl sm:rounded-2xl shadow-2xl hover:shadow-white/30 transform hover:scale-105 transition duration-300 cursor-pointer"
-      >
-        Try Demo Now →
+      >{loading ? (
+          <span className="flex items-center">
+            <svg className="mr-3 h-5 w-5 animate-spin text-white" viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+            </svg>
+            Loading...
+          </span>
+        ) : (
+          'Try Demo Now →'
+        )}
       </button>
 
       <button
--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -100,7 +100,7 @@ export default function Dashboard() {
       <div className="max-w-7xl mx-auto p-4 sm:p-6 md:p-8 ">
         <div className="mb-6 sm:mb-8 flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 pt-16 sm:pt-22">
           <p className="text-2xl sm:text-3xl font-bold text-gray-800">
-            {userName} Notes
+            Hello,{userName}
           </p>
           <button
             onClick={() => router.push("/notes/new")}
